814e63395e5774daca97c1b5de537db72d9674d9
tomek <tomekzbroszczyk@gmail.com>
2024-06-02 14:36:00 +0200

    Admin view for categories and discounts

---
+++ src/main/java/com/example/merchstore/controllers/admin/ViewController.java
@@ -0,0 +1,53 @@
+package com.example.merchstore.controllers.admin;
+
+import com.example.merchstore.dto.Category;
+import com.example.merchstore.dto.Discount;
+import com.example.merchstore.repositories.CategoryRepository;
+import com.example.merchstore.repositories.DiscountRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDate;
+import java.util.List;
+
+/**
+ * @author Tomasz Zbroszczyk
+ * @version 1.0
+ * @since 02.06.2024
+ */
+
+@Controller
+@RequestMapping("/api/admin/view")
+public class ViewController {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Autowired
+    private DiscountRepository discountRepository;
+
+    @GetMapping("/categories")
+    public String getAllCategories(Model model) {
+        List<Category> categories = categoryRepository.findAll();
+        model.addAttribute("categories", categories);
+        return "viewCategories";
+    }
+
+    @GetMapping("/discounts")
+    public String getAllDiscounts(Model model) {
+        List<Discount> discounts = discountRepository.findAll();
+        model.addAttribute("discounts", discounts);
+        return "viewDiscounts";
+    }
+
+    @GetMapping("/discounts/valid")
+    public String getValidDiscounts(Model model) {
+        List<Discount> validDiscounts = discountRepository.findAllValidDiscounts(LocalDate.now());
+        model.addAttribute("discounts", validDiscounts);
+        return "viewDiscounts";
+    }
+}
--- src/main/java/com/example/merchstore/repositories/DiscountRepository.java
+++ src/main/java/com/example/merchstore/repositories/DiscountRepository.java
@@ -2,6 +2,11 @@ package com.example.merchstore.repositories;
 
 import com.example.merchstore.dto.Discount;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
+import java.util.List;
 
 /**
  * @author Tomasz Zbroszczyk
@@ -12,4 +17,6 @@ public interface DiscountRepository extends JpaRepository<Discount, Long> {
 
     public Discount findByCode(String code);
 
+    @Query("SELECT d FROM Discount d WHERE d.validUntil >= :today AND d.validFrom <= :today")
+    List<Discount> findAllValidDiscounts(@Param("today") LocalDate today);
 }
